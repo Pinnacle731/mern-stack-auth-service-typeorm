5a7229b1edea8123325a42280b7f3b10a72145ed
Jawaliya123 <parthdangroshiya1895@gmail.com>
2025-01-16 14:12:00 +0530

    test cases time duration increase

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,5 @@ module.exports = {
   collectCoverage: true,
   coverageProvider: 'v8',
   collectCoverageFrom: ['src/**/*.ts', '!tests/**', '!**/node_modules/**'],
+  testTimeout: 10000,
 };
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,6 +7,8 @@ import logger from '../config/logger';
 import createHttpError from 'http-errors';
 
 export const AppDataSource = async (): Promise<DataSource | undefined> => {
+  logger.info('database app datasource calling');
+
   try {
     // Load the SSL certificate synchronously
     const rdsSSL = await getFileFromS3(
